4454da2c4f17978af7af42c3188e5cc0e3b869fc
leeyegy <leeyegy@gmail.com>
2020-08-13 22:54:05 +0800

    by update.sh

--- src/test_linf.sh
+++ src/test_linf.sh
@@ -1,7 +1,10 @@
 for resume_path in blend_FGSM_0.03137_model_063.pth  surrogate_FGSM_0.03137_model_062.pth std_training_model_062.pth no_surrogate_FGSM_0.03137_model_120.pth adv_training_PGD_0.03137_model_116.pth adv_training_FGSM_0.03137_model_089.pth
 do
 for epsilon in 0.00784 0.03137 0.06275
-do 
-python main.py --fd-eta 0.1 --max-queries 10000 --image-lr 0.005 --mode linf --online-lr 100 --exploration 0.01 --epsilon $epsilon --batch-size 500 --gradient-iters 1  --resume_path checkpoint/$resume_path | tee log/$epsilon/$resume_path.txt
+do
+for max in 1000 5000
+do  
+python main.py --fd-eta 0.1 --max-queries $max --image-lr 0.005 --mode linf --online-lr 100 --exploration 0.01 --epsilon $epsilon --batch-size 500 --gradient-iters 1  --resume_path checkpoint/$resume_path | tee log/$epsilon/max_queries_$max\_$resume_path.txt
+done
 done
 done
